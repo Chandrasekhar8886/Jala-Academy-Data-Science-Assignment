40892e1f2bb65aa5a135df50aa13f04137e0434c
Chandrasekhar88 <chandrasekharreddy8886@gmail.com>
2026-06-10 11:11:03 +0530

    Array Manipulation/flatten 3x4 array.py,Array Manipulation/reshape 1d array into 3x4.py

---
+++ Array Manipulation/flatten 3x4 array.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+
+flattened_array = matrix.flatten()
+
+print("Original Matrix:")
+print(matrix)
+
+print("\nFlattened Array:")
+print(flattened_array)
---
+++ Array Manipulation/reshape 1d array into 3x4.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+matrix = arr.reshape(3, 4)
+
+print("Original 1D Array:")
+print(arr)
+
+print("\n3x4 Matrix:")
+print(matrix)
