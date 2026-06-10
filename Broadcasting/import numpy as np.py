59be79b2f27e15b7d981edf88543d4477cbc0c97
Chandrasekhar88 <chandrasekharreddy8886@gmail.com>
2026-06-10 11:43:30 +0530

    Reorganized assignment folders

---
+++ Broadcasting/import numpy as np.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+arr = np.array([10, 20, 30])
+
+result = matrix + arr
+
+print("Original Matrix:")
+print(matrix)
+
+print("\n1x3 Array:")
+print(arr)
+
+print("\nResult after Broadcasting:")
+print(result)
